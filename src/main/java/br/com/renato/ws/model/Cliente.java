4721ad58cb2f810a302e0b0404bce31c69b6a7c3
Renato <renato.mello@paulistano.org.br>
2016-07-04 16:26:04 -0300

    Adicionado README.md

---
+++ src/main/java/br/com/renato/ws/model/Cliente.java
@@ -0,0 +1,27 @@
+package br.com.renato.ws.model;
+
+/**
+ * Created by renato on 04/07/16.
+ */
+public class Cliente {
+
+    private Integer id;
+
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
